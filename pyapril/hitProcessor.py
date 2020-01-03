5a5d5d08f5e66a78f19805947305e6d20f815a15
Tamas Peto <petotax@gmail.com>
2020-01-03 17:02:19 +0100

    [ADD] Target DOA estimation from the range-Doppler maps of the surveillance channels

--- pyapril/hitProcessor.py
+++ pyapril/hitProcessor.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as plt
+from pyargus import directionEstimation as de
 """
 
                              Python based Advanced Passive Radar Library (pyAPRiL)
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
      ------------
          Contains the implementation of the most common hit processing algorithms.
                   
-             - CA-CFAR processor: Implements an automatic detection with (Cell Averaging - Constant False Alarm Rate) detection.             
+             - CA-CFAR processor: Implements an automatic detection with (Cell Averaging - Constant False Alarm Rate) detection.
+             - Target DOA estimator: Estimates direction of arrival for the target reflection from the range-Doppler
+                                     maps of the surveillance channels using phased array techniques.
         
      Notes:
      ------------
@@ -21,14 +23,13 @@ import matplotlib.pyplot as plt
      ------------
 
      Project: pyAPRIL
-
      Authors: Tamás Pető
-
      License: GNU General Public License v3 (GPLv3)
 
      Changelog :
          - Ver 1.0.0    : Initial version (2017 11 02)
          - Ver 1.0.1    : Faster CFAR implementation(2019 02 15)
+         - Ver 1.1.0    : Target DOA estimation (2019 04 11)
 
  """
 
@@ -42,7 +43,7 @@ def CA_CFAR(rd_matrix, win_param, threshold):
         The threshold level is determined for each cell in the range-Doppler map with the estimation
         of the power level of its surrounding noise. The average power of the noise is estimated on a 
         rectangular window, that is defined around the CUT (Cell Under Test). In order the mitigate the effect
-        of the target reflecion energy spreading some cells are left out from the calculation in the inmediate
+        of the target reflection energy spreading some cells are left out from the calculation in the immediate
         vicinity of the CUT. These cells are the guard cells.
         The size of the estimation window and guard window can be set with the win_param parameter. 
     
@@ -106,4 +107,52 @@ def CA_CFAR(rd_matrix, win_param, threshold):
                 
     return hit_matrix
 
+
+def target_DOA_estimation(rd_maps, hit_list, DOA_method, array_alignment):
+    """
+        Performs DOA (Direction of Arrival) estimation for the given hits. To speed up the calculation for multiple
+        hits this function requires the calculated range-Doppler maps from all the surveillance channels.
+
+    Parameters:
+    -----------
+        :param: rd_maps: range-Doppler matrices from which the azimuth vector can be extracted
+        :param: hit_list: Contains the delay and Doppler coordinates of the targets.
+        :param: DOA_method: Name of the required algorithm to use for the estimation
+        :param: array_alignment: One dimensional array, which describes the active antenna positions
+
+        :type : rd_maps: complex valued numpy array with the size of  Μ x D x R , where R is equal to
+                                the number of range cells, and D denotes the number of Doppler cells.
+        :type: hit_list: Python list [[delay1, Doppler1],[delay2, Doppler2]...].
+        :type: DOA_method: string
+        :type: array_alignment: real valued numpy array with size of 1 x M, where M is the number of
+                            surveillance antenna channels.
+
+    Return values:
+    --------------
+        target_doa : Measured incident angles of the targets
+
+    TODO: Extend with decorrelation support
+    """
+    doa_list = []  # This list will contains the measured DOA values
+
+    # Generate scanning vectors
+    thetas = np.arange(0, 180, 1)
+    scanning_vectors = de.gen_ula_scanning_vectors(array_alignment, thetas)
+
+    for hit in hit_list:
+        azimuth_vector = rd_maps[:, hit[1], hit[0]]
+        R = np.outer(azimuth_vector, azimuth_vector.conj())
+        if DOA_method == "Fourier":
+            doa_res = de.DOA_Bartlett(R, scanning_vectors)
+        elif DOA_method == "Capon":
+            doa_res = de.DOA_Capon(R, scanning_vectors)
+        elif DOA_method == "MEM":
+            doa_res = de.DOA_MEM(R, scanning_vectors, column_select=0)
+        elif DOA_method == "MUSIC":
+            doa_res = de.DOA_MUSIC(R, scanning_vectors, signal_dimension=1)
+
+        hit_doa = thetas[np.argmax(doa_res)]
+        doa_list.append(hit_doa)
+        print("Estimating DOA for hit-> r:{:d} D:{:.2f} Az:{:.2f} ".format(hit[0], hit[1], hit_doa))
+    return doa_list
 
